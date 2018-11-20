3c0d578d43998107514bb7bec2d88d414965245b
k-soda <>
2018-11-20 22:38:50 +0900

    Initial Commit

---
+++ SideMenuSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuSample
+//
+//  Created by KS on 2018/11/20.
+//  Copyright © 2018 KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
